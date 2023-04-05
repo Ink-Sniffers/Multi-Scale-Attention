734806eaa511a4523cea1f99d7e18053ab10c2d0
Jamessmith9956 <jamessmith9956@gmail.com>
2023-04-05 22:34:14 +1000

    setup training settings to find data in ../data/

--- train.sh
+++ train.sh
@@ -1,1 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python src/main.py --modelName "MS-Dual-Guided" --batch_size 8 --epochs 500 --lr 1e-3            
+#CUDA_VISIBLE_DEVICES=0 python src/main.py --modelName "MS-Dual-Guided" --batch_size 8 --epochs 5 --lr 1e-3            
+CUDA_VISIBLE_DEVICES=0 python src/main.py --modelName "MS-Dual-Guided" --root "../data/" --batch_size 8 --epochs 5 --lr 1e-3   
